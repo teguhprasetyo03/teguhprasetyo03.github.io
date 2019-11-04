b41726a003522baf05a8fcefd5019fc390ff2e2a
teguhprasetyo <teguhprasetyo261211@gmail.com>
2019-11-04 20:46:27 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+    $('#1').fadeOut();
+    $('#2').fadeOut();
+    $('#3').fadeOut();
+    $('#4').fadeOut();
+    $('#5').fadeOut();
+    $('#6').fadeOut();
+    $('#7').fadeOut();
+    $('#8').fadeOut();
+
+
+})
